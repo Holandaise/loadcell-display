ea1440b50e5e939d995ac16d947c351487f4980c
Adam Montgomery <adamlmontgomery@gmail.com>
2021-07-20 15:14:15 -0400

    Added new branch for SPI communication to display

--- loadcell-display.ino
+++ loadcell-display.ino
@@ -1,10 +1,13 @@
 #include <HX711.h>
+#include <SPI.h>
 
 #define LOADCELL_DOUT 2
 #define LOADCELL_SCK 3
 
+#define SSB 4 // SPI slave select
+
 HX711 scale; // global scale object
-float calibration_factor = -45000; // this is gathered from calibration sketch
+float calibration_factor = -8000; // this is gathered from calibration sketch
 
 typedef union
 {
@@ -15,18 +18,37 @@ typedef union
 void display(int val) {
   Serial.write(0x76); // clear display
   char str[5] = {0};
-  //str[4] = '\0';
   sprintf(str, "%4d", val);
   Serial.write(0x77); // Decimal control
   Serial.write(0b00000100); // Set 3rd digit decimal on
   Serial.print(str);
 }
 
+void displaySPI(int val) {
+  char str[4] = {0}; //initialize an array of bytes
+  sprintf(str, "%4d", val); // format val into str
+  digitalWrite(SSB, LOW); // activate slave device
+  SPI.transfer(0x76); // clear display
+  SPI.transfer(0x77); // decimal control
+  SPI.transfer(0x04); // set 3rd decimal on
+
+  // transmit each byte in str 1 at a time
+  for (int i = 0; i < 4; i++) {
+    SPI.transfer(str[i]);
+  }
+  digitalWrite(SSB, HIGH); // deactivate slave device
+}
+
 void setup() {
   // put your setup code here, to run once:
   scale.begin(LOADCELL_DOUT, LOADCELL_SCK);
-  Serial.begin(9600);
-  Serial.write(0x76); //clear display
+  // Serial.begin(9600);
+  pinMode(SSB, OUTPUT);
+  digitalWrite(SSB, HIGH); // make sure SS is high
+  // Serial.write(0x76); //clear display
+  SPI.begin();
+  SPI.setClockDivider(SPI_CLOCK_DIV64); // slow down SPI transfers
+
   scale.set_scale(calibration_factor);
   delay(250);
   scale.tare();
@@ -44,6 +66,7 @@ int apply_mask(float reading){
 void loop() {
   float reading = scale.get_units();
   int value = apply_mask(reading);
-  display(value);
+  // display(value);
+  displaySPI(value);
   delay(500); // hold display for visibility
 }
